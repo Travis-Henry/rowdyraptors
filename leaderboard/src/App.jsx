9e88b0067c85ea6a818fa832d37aa2386bfa4798
Travis-Henry <travis.henry2016@gmail.com>
2024-02-25 10:33:26 -0700

    cleaned up comments

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
-// import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 
 function App() {
   const testList = ["item 1", "item 2", "item 3"];
@@ -13,46 +12,6 @@ function App() {
     ["Player 3", "4", "6"],
     // Add more players and scores as needed
   ];
-  //const [allScores, setAllScores] = useState([]);
-
-  // const client = new KintoneRestAPIClient({
-  //   baseUrl: "https://dino-hacks.kintone.com",
-  //   //   // Use password authentication
-  //   //   // auth: {
-  //   //   //   username: process.env.KINTONE_USERNAME,
-  //   //   //   password: process.env.KINTONE_PASSWORD,
-  //   //   // },
-  //   //   //Use API token authentication
-  //   auth: { apiToken: "S3N65FATzOFoob0bgr40vcBVccyKiGVLFUdw8N1M" },
-  //   //   //Use OAuth token authentication
-  //   //   //auth: { oAuthToken: process.env.KINTONE_OAUTH_TOKEN }
-
-  //   //   // Use session authentication if `auth` is omitted (in browser only)
-  // });
-
-  // client.record
-  //   .getRecords({ app: "3", id: "2" })
-  //   .then((resp) => {
-  //     console.log(resp.records);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  // useEffect(() => {
-  //   axios
-  //     .get(`https://dino-hacks.kintone.com/k/v1/record.json`, {
-  //       headers: {
-  //         "X-Cybozu-API-Token": "S3N65FATzOFoob0bgr40vcBVccyKiGVLFUdw8N1M",
-  //         "id": "2",
-  //         "app": "3",
-  //       },
-  //     })
-  //     .then((res) => {
-  //       console.log("Test data");
-  //       console.log(res.data);
-  //     })
-  //     .catch((error) => console.log(error + "     Test"));
-  // }, []);
 
   return (
     <div className="scoreboard">
